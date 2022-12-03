e07f5d4e43e56419bf7ff3184e7c041ad8791fce
muhammadfayyaz <muhammadfayyaz3278@gmail.com>
2022-12-03 21:09:49 +0500

    Todo App complete with url change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const addItem =()=>{
   let todoItem = document.getElementById("todoItem");
   console.log(todoItem.value)
-  axios.post("http://localhost:5001/todo", {text : todoItem.value})
+  axios.post("https://todowithnodesever.up.railway.app/todo", {text : todoItem.value})
   .then(response =>{
     document.getElementById("result").innerHTML = "";
     response.data.todos.map((item,index)=>{
@@ -22,7 +22,7 @@ const addItem =()=>{
 
 
 const getTodos = abc =>{
-  axios.get("http://localhost:5001/todos")
+  axios.get("https://todowithnodesever.up.railway.app/todos")
   .then(response=>{
     document.getElementById("result").innerHTML = "";
     response.data.todos.map((item,index)=>{
